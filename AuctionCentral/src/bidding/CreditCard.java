766b3fa68e24636b44efe533ec7cfca457653bb0
luenekw@uw.edu <luenekw@uw.edu@2b68a43b-548d-a8f3-15b1-0ecbf99f8650>
2014-02-27 06:20:45 +0000

    Changes to CreditCard, and CreditCardTest

--- AuctionCentral/src/bidding/CreditCard.java
+++ AuctionCentral/src/bidding/CreditCard.java
@@ -5,269 +5,298 @@ import java.util.Date;
 /**
  * Class: CreditCard
  * 
- * Represents a credit card that a bidder will use to pay for a
- * item that they win the bidding for.
+ * Represents a credit card that a bidder will use to pay for a item that they
+ * win the bidding for.
  * 
- * TCSS 360 - Software Development and Quality Assurance
- * University of Washington, Tacoma
- * Winter 2014
- * Instructor: Dr. Adwoa Donyina
+ * TCSS 360 - Software Development and Quality Assurance University of
+ * Washington, Tacoma Winter 2014 Instructor: Dr. Adwoa Donyina
  * 
  * @author Keith Lueneburg
- *
+ * @version 2/26/2014
+ * 
  */
 public final class CreditCard {
-	/**
-	 * The 16 digit card number.
-	 */
-	private long myCardNum;
-	
-	/**
-	 * Card expiration date.
-	 */
-	private Date myExpDate;
-	
-	/**
-	 * The 3 digit card security code.
-	 */
-	private int myCSC;
-	
-	/**
-	 * The name of the card holder.
-	 */
-	private String myCardHolder;	
-	
-	/**
-	 * The card holder's address.
-	 */
-	private Address myAddress;
-	
-	/**
-	 * The bank this card belongs to.
-	 */
-	private Bank myBank;	
-		
-	/**
-	 * Create a new CreditCard object.
-	 * <p>
-	 * <dt><b> Precondition: Valid parameters are passed to 
-	 * constructor. </b><dd>
-	 * <dt><b> Postcondition: The CreditCard is initialized 
-	 * properly. </b><dd>
-	 * 
-	 * @param aCardNum The card number.
-	 * @param anExpDate The expiration date.
-	 * @param aCSC The CSC code.
-	 * @param aCardHolder The card holder.
-	 * @param anAddress The card holder's address.
-	 * @param aBank The bank the card belongs to.
-	 * 
-	 * @return The CreditCard object.
-	 * @throws IllegalArgumentException Throws exception in the case of
-	 * invalid parameters.
-	 */
-	public CreditCard(long aCardNum, Date anExpDate, int aCSC,
-			String aCardHolder, Address anAddress, Bank aBank){
-		
-		// Validation is handled by individual setters
-		setCardNum(aCardNum);
-		setExpDate(anExpDate);
-		setCSC(aCSC);
-		setCardHolder(aCardHolder);
-		setAddress(anAddress);
-		setBank(aBank);
-	}
-	
-	/**
-	 * @return The card number.
-	 */
-	private final long getCardNum() {
-		return myCardNum;
-	}
+  /**
+   * Acceptable length of credit card number.
+   */
+  private static final int CARD_NUMBER_LENGTH = 16;
 
-	/**
-	 * @return The expiration date.
-	 */
-	private final Date getExpDate() {
-		
-		// TODO : return a defensive copy here
-		return myExpDate;
-	}
+  /**
+   * Acceptable length of CSC code.
+   */
+  private static final int CSC_LENGTH = 3;
 
-	/**
-	 * @return The CSC code.
-	 */
-	private final int getCSC() {
-		return myCSC;
-	}
+  /**
+   * The 16 digit card number.
+   */
+  private long my_card_num;
 
-	/**
-	 * @return The card holder name.
-	 */
-	private final String getCardHolder() {
-		return myCardHolder;
-	}
+  /**
+   * Card expiration date.
+   */
+  private Date my_expiration_date;
 
-	/**
-	 * @return The address.
-	 */
-	private final Address getAddress() {
-		
-		//TODO : return a defensive copy once Address is complete.
-		return myAddress;
-	}
+  /**
+   * The 3 digit card security code.
+   */
+  private int my_csc;
 
-	/**
-	 * @return The bank.
-	 */
-	private final Bank getBank() {
-		return myBank;
-	}
-	
-	/**
-	 * @param aCardNum The card number to set.
-	 */
-	private final void setCardNum(long aCardNum) {
-		if((new Long(aCardNum)).toString().length() != 16) {
-			throw new IllegalArgumentException("Invalid card number length");
-		} else {
-			this.myCardNum = aCardNum;
-		}
-	}
+  /**
+   * The name of the card holder.
+   */
+  private String my_card_holder;
 
-	/**
-	 * @param anExpDate The expiration date to set.
-	 */
-	private final void setExpDate(Date anExpDate) {
-		
-		//TODO : make defensive copy
-		this.myExpDate = anExpDate;
-	}
+  /**
+   * The card holder's address.
+   */
+  private Address my_address;
 
-	/**
-	 * @param aCSC The CSC to set.
-	 */
-	private final void setCSC(int aCSC) {
-		if((new Integer(aCSC)).toString().length() != 3) {
-			throw new IllegalArgumentException("Invalid CSC length");
-		} else {
-			this.myCSC = aCSC;
-		}
-	}
+  /**
+   * The bank this card belongs to.
+   */
+  private Bank my_bank;
 
-	/**
-	 * <p>
-	 * <dt><b> Precondition: Card holder name is not blank. </b><dd>
-	 * <dt><b> Postcondition: Name is set. </b><dd>
-	 * 
-	 * @param aCardHolder The card holder name to set.
-	 */
-	private final void setCardHolder(String aCardHolder) {
-		if (aCardHolder.length() == 0) {
-			throw new IllegalArgumentException("Name cannot be blank");
-		} else {
-			this.myCardHolder = aCardHolder;
-		}
-	}
+  /**
+   * Create a new CreditCard object.
+   * <p>
+   * <dt><b> Precondition: Valid parameters are passed to constructor. </b>
+   * <dd>
+   * <dt><b> Postcondition: The CreditCard is initialized properly. </b>
+   * <dd>
+   * 
+   * @param a_card_num
+   *          The card number.
+   * @param an_exp_date
+   *          The expiration date.
+   * @param a_csc
+   *          The CSC code.
+   * @param a_card_holder
+   *          The card holder.
+   * @param an_address
+   *          The card holder's address.
+   * @param a_bank
+   *          The bank the card belongs to.
+   * 
+   * @throws IllegalArgumentException
+   *           Throws exception in the case of invalid parameters.
+   */
+  public CreditCard(final long a_card_num, final Date an_exp_date,
+      final int a_csc, final String a_card_holder, final Address an_address,
+      final Bank a_bank) {
 
-	/**
-	 * @param anAddress The address to set.
-	 */
-	private final void setAddress(Address anAddress) {
-		
-		//TODO : make defensive copy
-		this.myAddress = anAddress;
-	}
+    // Validation is handled by individual setters
+    setCardNum(a_card_num);
+    setExpDate(an_exp_date);
+    setCSC(a_csc);
+    setCardHolder(a_card_holder);
+    setAddress(an_address);
+    setBank(a_bank);
+  }
 
-	/**
-	 * @param aBank The bank name to set.
-	 */
-	private final void setBank(Bank aBank) {
+  /**
+   * @return The card number.
+   */
+  private long getCardNum() {
+    return my_card_num;
+  }
 
-		//TODO : make defensive copy
-		this.myBank = aBank;
-	}
+  /**
+   * @return The expiration date.
+   */
+  private Date getExpDate() {
 
-	/**
-	 * Check to see if this CreditCard matches another CreditCard.
-	 * <p>
-	 * <dt><b> Precondition: CreditCard has been initialized. </b><dd>
-	 * <dt><b> Postcondition: The CreditCard has not been changed. </b><dd>
-	 * 
-	 * @param anObject The object to compare this CreditCard with.
-	 * 
-	 * @return True if the other object is also a CreditCard, and it represents
-	 * the same card as the card we are checking.
-	 */
-	@Override
-	public boolean equals(Object anObject) {
-		boolean isEqual = true;
-		
-		// Check for null object
-		if (anObject == null) {
-			isEqual = false;
-			
-		// Check for correct object type
-		} else if (!(anObject instanceof CreditCard)) {
-			isEqual = false;
-		} else {
-			
-			// Cast other object to a CreditCard
-			CreditCard other = (CreditCard)anObject;
-		
-			// Compare field by field for equality
-			if (!myCardHolder.equals(other.getCardHolder())) {
-				isEqual = false;
-			} else if (myCardNum != other.getCardNum()) {
-				isEqual = false;
-			} else if (!myExpDate.equals(other.getExpDate())) {
-				isEqual = false;
-			} else if (myCSC != other.getCSC()) {
-				isEqual = false;
-			} else if (!myAddress.equals(other.getAddress().toString())) {
-				isEqual = false;
-			} else if (!myBank.equals(other.getBank())) {
-				isEqual = false;
-			}
-		}
-		
-		return isEqual;
-	}
-	
-	/**
-	 * Get a String representation of the CreditCard.
-	 * <p>
-	 * <dt><b> Precondition: CreditCard has been initialized. </b><dd>
-	 * <dt><b> Postcondition: The CreditCard has not been changed. </b><dd>
-	 * 
-	 * @return A string representation of the card.
-	 */
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Cardholder: ");
-		sb.append(myCardHolder);
-		sb.append("\n");
-		sb.append("Card Number: ");
-		sb.append(myCardNum);
-		sb.append("\n");
-		sb.append("Expiration Date: ");
-		sb.append(myExpDate.getDay() + " ");
-		sb.append("\n");
-		sb.append("CSC: ");
-		sb.append(myCSC);
-		sb.append("\n");
-		sb.append("Address: ");
-		
-		//TODO : once Address toString() is implemented, use that here!
-		sb.append("PLACEHOLDER ADDRESS");
-		
-		sb.append("\n");
-		sb.append("Bank: ");
-		//TODO : once Address toString() is implemented, use that here!
-		sb.append("PLACEHOLDER BANK");
-		sb.append("\n");
-		
-		return sb.toString();
-	}
+    // TODO : return a defensive copy here
+    return my_expiration_date;
+  }
+
+  /**
+   * @return The CSC code.
+   */
+  private int getCSC() {
+    return my_csc;
+  }
+
+  /**
+   * @return The card holder name.
+   */
+  private String getCardHolder() {
+    return my_card_holder;
+  }
+
+  /**
+   * @return The address.
+   */
+  private Address getAddress() {
+
+    // TODO : return a defensive copy once Address is complete.
+    return my_address;
+  }
+
+  /**
+   * @return The bank.
+   */
+  private Bank getBank() {
+    return my_bank;
+  }
+
+  /**
+   * @param a_card_num
+   *          The card number to set.
+   */
+  private void setCardNum(final long a_card_num) {
+    if ((new Long(a_card_num)).toString().length() != CARD_NUMBER_LENGTH) {
+      throw new IllegalArgumentException("Invalid card number length");
+    } else {
+      this.my_card_num = a_card_num;
+    }
+  }
+
+  /**
+   * @param an_expiration_date
+   *          The expiration date to set.
+   */
+  private void setExpDate(final Date an_expiration_date) {
+
+    // TODO : make defensive copy
+    this.my_expiration_date = an_expiration_date;
+  }
+
+  /**
+   * @param a_csc
+   *          The CSC to set.
+   */
+  private void setCSC(final int a_csc) {
+    if ((new Integer(a_csc)).toString().length() != CSC_LENGTH) {
+      throw new IllegalArgumentException("Invalid CSC length");
+    } else {
+      this.my_csc = a_csc;
+    }
+  }
+
+  /**
+   * <p>
+   * <dt><b> Precondition: Card holder name is not blank. </b>
+   * <dd>
+   * <dt><b> Postcondition: Name is set. </b>
+   * <dd>
+   * 
+   * @param a_card_holder
+   *          The card holder name to set.
+   */
+  private void setCardHolder(final String a_card_holder) {
+    if (a_card_holder.length() == 0) {
+      throw new IllegalArgumentException("Name cannot be blank");
+    } else {
+      this.my_card_holder = a_card_holder;
+    }
+  }
+
+  /**
+   * @param an_address
+   *          The address to set.
+   */
+  private void setAddress(final Address an_address) {
+
+    // TODO : make defensive copy
+    this.my_address = an_address;
+  }
+
+  /**
+   * @param a_bank
+   *          The bank name to set.
+   */
+  private void setBank(final Bank a_bank) {
+
+    // TODO : make defensive copy
+    this.my_bank = a_bank;
+  }
+
+  /**
+   * Check to see if this CreditCard matches another CreditCard.
+   * <p>
+   * <dt><b> Precondition: CreditCard has been initialized. </b>
+   * <dd>
+   * <dt><b> Postcondition: The CreditCard has not been changed. </b>
+   * <dd>
+   * 
+   * @param an_object
+   *          The object to compare this CreditCard with.
+   * 
+   * @return True if the other object is also a CreditCard, and it represents
+   *         the same card as the card we are checking.
+   */
+  @Override
+  public boolean equals(final Object an_object) {
+    boolean is_equal = true;
+
+    // Check for null object
+    if (an_object == null) {
+      is_equal = false;
+
+      // Check for correct object type
+    } else if (!(an_object instanceof CreditCard)) {
+      is_equal = false;
+    } else {
+
+      // Cast other object to a CreditCard
+      final CreditCard other = (CreditCard) an_object;
+
+      // Compare field by field for equality
+      if (!my_card_holder.equals(other.getCardHolder())) {
+        is_equal = false;
+      } else if (my_card_num != other.getCardNum()) {
+        is_equal = false;
+      } else if (!my_expiration_date.equals(other.getExpDate())) {
+        is_equal = false;
+      } else if (my_csc != other.getCSC()) {
+        is_equal = false;
+      } else if (!my_address.equals(other.getAddress().toString())) {
+        is_equal = false;
+      } else if (!my_bank.equals(other.getBank())) {
+        is_equal = false;
+      }
+    }
+
+    return is_equal;
+  }
+
+  /**
+   * Get a String representation of the CreditCard.
+   * <p>
+   * <dt><b> Precondition: CreditCard has been initialized. </b>
+   * <dd>
+   * <dt><b> Postcondition: The CreditCard has not been changed. </b>
+   * <dd>
+   * 
+   * @return A string representation of the card.
+   */
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("Cardholder: ");
+    sb.append(my_card_holder);
+    sb.append("\n");
+    sb.append("Card Number: ");
+    sb.append(my_card_num);
+    sb.append("\n");
+    sb.append("Expiration Date: ");
+    sb.append(my_expiration_date.getMonth() + "/" + my_expiration_date.getDay() + "/" 
+        + my_expiration_date.getYear());
+    sb.append("\n");
+    sb.append("CSC: ");
+    sb.append(my_csc);
+    sb.append("\n");
+    sb.append("Address: ");
+
+    // TODO : once Address toString() is implemented, use that here!
+    sb.append("PLACEHOLDER ADDRESS");
+
+    sb.append("\n");
+    sb.append("Bank: ");
+    // TODO : once Address toString() is implemented, use that here!
+    sb.append("PLACEHOLDER BANK");
+    sb.append("\n");
+
+    return sb.toString();
+  }
 }
--- AuctionCentral/src/test/CreditCardTest.java
+++ AuctionCentral/src/test/CreditCardTest.java
@@ -5,109 +5,122 @@ package test;
 
 import static org.junit.Assert.*;
 
+import bidding.Address;
+import bidding.Bank;
+import bidding.CreditCard;
+
 import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import bidding.Address;
-import bidding.Bank;
-import bidding.CreditCard;
+
+
 
 /**
  * Class: CreditCardTest
  * 
  * Contains tests for class: CreditCard
  * 
- * TCSS 360 - Software Development and Quality Assurance
- * University of Washington, Tacoma
- * Winter 2014
+ * TCSS 360 - Software Development and Quality Assurance 
+ * University of Washington, Tacoma 
+ * Winter 2014 
  * Instructor: Dr. Adwoa Donyina
  * 
  * @author Keith Lueneburg
- *
+ * @version 2/26/2014
+ * 
  */
 public class CreditCardTest {
 
-	static CreditCard card_1;
-	
-	static CreditCard card_1_duplicate;
-	
-	@Before
-	public void setup() {
-		
-		card_1 = new CreditCard(1234123412341234L, new Date(2015, 1, 15), 123,
-				"John Smith", new Address(), new Bank());
-		
-		card_1_duplicate = new CreditCard(1234123412341234L, new Date(2015, 1, 15), 123,
-				"John Smith", new Address(), new Bank());
-	}
-	
-	/**
-	 * Test method for {@link bidding.CreditCard#CreditCard(long, int, int, java.lang.String, bidding.Address, java.lang.String)}.
-	 */
-	@Test
-	public void testCreditCard() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Test credit card.
+   */
+  private static CreditCard card_1;
 
-	/**
-	 * Test method for {@link bidding.CreditCard#equals(java.lang.Object)}.
-	 */
-	@Test
-	public void testEqualsObject() {
-		// Object should equal itself
-		assertTrue(card_1.equals(card_1));
-		
-		// Object should equal an identical object with same field values
-		// TODO: This currently fails due to Address and Bank not being
-		// implemented yet.
-		
-		assertTrue(card_1.equals(card_1_duplicate));
-		
-		// Should not equal a null object
-		assertFalse(card_1.equals(null));
-		
-		// Should not equal a different type of object
-		assertFalse(card_1.equals(new StringBuilder()));
-		
-		// Should not equal a card with different fields:
-		
-		// Different card number:
-		assertFalse(card_1.equals(new CreditCard(4567123412341234L, 
-				new Date(2015, 1, 15), 123, "John Smith", new Address(),
-				new Bank())));
-		
-		// Different expiration date:
-		assertFalse(card_1.equals(new CreditCard(1234123412341234L, 
-				new Date(2238, 1, 15), 123, "John Smith", new Address(),
-				new Bank())));
-		
-		// Different CSC
-		assertFalse(card_1.equals(new CreditCard(1234123412341234L, 
-				new Date(2015, 1, 15), 456, "John Smith", new Address(),
-				new Bank())));
-		
-		// Different CSC
-		assertFalse(card_1.equals(new CreditCard(1234123412341234L, 
-				new Date(2015, 1, 15), 123, "Dave Jones", new Address(),
-				new Bank())));
-		
-		// Different Address
-		
-			//TODO: Test once Address implemented
-			
-		// Different Bank
-		
-			//TODO: Test once Bank implemented
-	}
+  /**
+   * Test credit card, identical to 'card_1'.
+   */
+  private static CreditCard card_1_duplicate;
 
-	/**
-	 * Test method for {@link bidding.CreditCard#toString()}.
-	 */
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Setup up prior to each test.
+   */
+  @Before
+  public void setup() {
+
+    card_1 = new CreditCard(1234123412341234L, new Date(2015, 1, 15), 123,
+        "John Smith", new Address(), new Bank());
+
+    card_1_duplicate = new CreditCard(1234123412341234L, new Date(2015, 1, 15),
+        123, "John Smith", new Address(), new Bank());
+  }
+
+  /**
+   * Test method for
+   * {@link bidding.CreditCard#CreditCard(long, int, int, java.lang.String, 
+   *    bidding.Address, java.lang.String)}
+   * .
+   */
+  @Test
+  public void testCreditCard() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link bidding.CreditCard#equals(java.lang.Object)}.
+   */
+  @Test
+  public void testEqualsObject() {
+    // Object should equal itself
+    assertTrue(card_1.equals(card_1));
+
+    // Object should equal an identical object with same field values
+    // TODO: This currently fails due to Address and Bank not being
+    // implemented yet.
+
+    assertTrue(card_1.equals(card_1_duplicate));
+
+    // Should not equal a null object
+    final CreditCard null_card = null;
+    assertFalse(card_1.equals(null_card));
+
+    // Should not equal a different type of object
+    assertFalse(card_1.equals(new StringBuilder()));
+
+    // Should not equal a card with different fields:
+
+    // Different card number:
+    assertFalse(card_1.equals(new CreditCard(4567123412341234L, new Date(2015,
+        1, 15), 123, "John Smith", new Address(), new Bank())));
+
+    // Different expiration date:
+    assertFalse(card_1.equals(new CreditCard(1234123412341234L, new Date(2238,
+        1, 15), 123, "John Smith", new Address(), new Bank())));
+
+    // Different CSC
+    assertFalse(card_1.equals(new CreditCard(1234123412341234L, new Date(2015,
+        1, 15), 456, "John Smith", new Address(), new Bank())));
+
+    // Different CSC
+    assertFalse(card_1.equals(new CreditCard(1234123412341234L, new Date(2015,
+        1, 15), 123, "Dave Jones", new Address(), new Bank())));
+
+    // Different Address
+
+    // TODO: Test once Address implemented
+
+    // Different Bank
+
+    // TODO: Test once Bank implemented
+  }
+
+  /**
+   * Test method for {@link bidding.CreditCard#toString()}.
+   */
+  @Test
+  public void testToString() {
+    fail("Not yet implemented");
+  }
 
 }
