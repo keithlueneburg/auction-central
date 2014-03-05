9d3b2bda39772d3b9af3ff76a1b44fcc9e8e62e4
kmalex90@uw.edu <kmalex90@uw.edu@2b68a43b-548d-a8f3-15b1-0ecbf99f8650>
2014-03-05 06:48:05 +0000

    Auction - Kevin - Some javadoc AuctionTest - Kevin - JUnit test cases

--- AuctionCentral/src/auction/Auction.java
+++ AuctionCentral/src/auction/Auction.java
@@ -4,26 +4,68 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
-
+/**
+ * 
+ * @author Kevin Alexander
+ * @version 2/28/2014
+ *
+ */
 public class Auction {
   
   /** The auction number, increases per instantiation of an auction class. */
   private static int AUCTION_NUMBER;
 
   // Used only when adding/removing items
+  /**
+   * 
+   */
   private int my_next_item;
+  /**
+   * 
+   */
   private int my_number;
-
+  /**
+   * 
+   */
   private String my_auction_name;
+  /**
+   * 
+   */
   private String my_contact_person;
+  /**
+   * 
+   */
   private String my_contact_phone;
+  /**
+   * 
+   */
   private String my_intake_person;
+  /**
+   * 
+   */
   private Calendar my_auction_date;
+  /**
+   * 
+   */
   private int my_auction_duration;
+  /**
+   * 
+   */
   private String my_comments;
-
+  /**
+   * 
+   */
   private List<Item> my_auction_items;
-
+  /**
+   * 
+   * @param the_auction_name
+   * @param the_contact_person
+   * @param the_contact_phone
+   * @param the_intake_person
+   * @param the_auction_date
+   * @param the_auction_duration
+   * @param the_comments
+   */
   public Auction(final String the_auction_name,
       final String the_contact_person, final String the_contact_phone,
       final String the_intake_person, final Calendar the_auction_date,
@@ -44,41 +86,69 @@ public class Auction {
     my_auction_items = new LinkedList<Item>();
 
   }
-
+  /**
+   * 
+   */
   public Auction() {
     this("", "", "", "", new GregorianCalendar(), 0, "");
   }
 
   // SETTERS
+  /**
+   * 
+   * @param the_auction_name
+   */
   public void setAuctionName(final String the_auction_name) {
     my_auction_name = the_auction_name;
   }
-
+  /**
+   * 
+   * @param the_contact_person
+   */
   public void setContactPerson(final String the_contact_person) {
     my_contact_person = the_contact_person;
   }
-
+  /**
+   * 
+   * @param the_contact_phone
+   */
   public void setContactPhone(final String the_contact_phone) {
     my_contact_phone = the_contact_phone;
   }
-
+  /**
+   * 
+   * @param the_intake_person
+   */
   public void setIntakePerson(final String the_intake_person) {
     my_intake_person = the_intake_person;
   }
-
+  /**
+   * 
+   * @param the_auction_date
+   */
   public void setAuctionDate(final Calendar the_auction_date) {
     my_auction_date = the_auction_date;
   }
-
+  /**
+   * 
+   * @param the_auction_duration
+   */
   public void setAuctionDuration(final int the_auction_duration) {
     my_auction_duration = the_auction_duration;
   }
-
+  /**
+   * 
+   * @param the_comments
+   */
   public void setComments(final String the_comments) {
     my_comments = the_comments;
   }
 
   // GETTERs
+  /**
+   * 
+   * @return
+   */
   public String getAuctionName() {
     return my_auction_name;
   }
@@ -97,37 +167,63 @@ public class Auction {
     String strI = sb.toString();
     return strI;
   }
-
+  /**
+   * 
+   * @return
+   */
   public String getContactPerson() {
     return my_contact_person;
   }
-
+  /**
+   * 
+   * @return
+   */
   public String getContactPhone() {
     return my_contact_phone;
   }
-
+  /**
+   * 
+   * @return
+   */
   public String getIntakePerson() {
     return my_intake_person;
   }
-
+  /**
+   * 
+   * @return
+   */
   public Calendar getAuctionDate() {
     return my_auction_date;
   }
-
+  /**
+   * 
+   * @return
+   */
   public int getAuctionDuration() {
     return my_auction_duration;
   }
-
+  /**
+   * 
+   * @return
+   */
   public String getComments() {
     return my_comments;
   }
-  
+  /**
+   * 
+   * @return
+   */
   public int getItemCount() {
     return my_auction_items.size();
   }
 
   // METHODS
-  boolean addItem(final Item the_item) {
+  /**
+   * This method adds the given item to the auction and increments the number of items. 
+   * @param the_item The Item object that is wanting to be added to the auction.
+   * @return A boolean to whether the item was added or not.
+   */
+  public boolean addItem(final Item the_item) {
     final boolean success = my_auction_items.add(the_item);
 
     if (success) {
@@ -138,7 +234,13 @@ public class Auction {
     return success;
   }
 
-  boolean deleteItem(final Item the_item) {
+  /**
+   * This method deletes the given item from the auction and decrements the number of 
+   * items.
+   * @param the_item The item object that is wanting to be deleted from the auction.
+   * @return A boolean to whether the item was added or not.
+   */
+  public boolean deleteItem(final Item the_item) {
     final boolean success = my_auction_items.remove(the_item);
 
     if (success) {
---
+++ AuctionCentral/src/test/AuctionTest.java
@@ -0,0 +1,58 @@
+package test;
+
+import auction.Auction;
+import auction.Item;
+
+import static org.junit.Assert.*;
+import java.util.GregorianCalendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuctionTest {
+  
+  Auction testAuction;
+  Item testItem;
+
+  @Before
+  public void setUp() throws Exception {
+    testAuction = new Auction();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testAuctionStringStringStringStringCalendarIntString() {
+    Auction testAuction2 = new Auction("", "", "", "", new GregorianCalendar(), 0, "");
+    assertTrue(testAuction2 != null);
+  }
+
+  @Test
+  public void testAuction() {
+    assertTrue(testAuction != null);
+  }
+
+  @Test
+  public void testAddItem() {
+    testAuction = new Auction();
+    
+    testItem = new Item();
+    testAuction.addItem(testItem);
+    assertEquals(1, testAuction.getItemCount());
+  }
+
+  @Test
+  public void testDeleteItem() {
+    Item testItem1 = new Item();
+    Item testItem2 = new Item();
+    testAuction.addItem(testItem1);
+    testAuction.addItem(testItem2);
+    assertEquals(2, testAuction.getItemCount());
+    testAuction.deleteItem(testItem2);
+    assertEquals(1, testAuction.getItemCount());
+  }
+
+}
