d83282bc73ae11a2697f747d2a0a2886cc328e41
luenekw@uw.edu <luenekw@uw.edu@2b68a43b-548d-a8f3-15b1-0ecbf99f8650>
2014-03-11 00:09:42 +0000

    Changes to registrationpanel

--- AuctionCentral/src/gui/RegistrationPanel.java
+++ AuctionCentral/src/gui/RegistrationPanel.java
@@ -22,7 +22,13 @@ import javax.swing.border.Border;
 import system.AuctionCentralSystem;
 import auction.Auction;
 
+
+/**
+ * @author luenekw
+ *
+ */
 @SuppressWarnings("serial")
+
 public class RegistrationPanel extends JPanel {
   /** The default height of the panel. */
   private static final int DEFAULT_HEIGHT = 680;
@@ -42,37 +48,68 @@ public class RegistrationPanel extends JPanel {
   /** The default font size for the instruction labels. */
   private static final int MAIN_FONT_SIZE = 24;
   
+  /** The default box height. */
+  private static final int BOX_HEIGHT = 63;
+
   /** The default box height for input spacing. */
-  private static final int BOX_HEIGHT_TWO = 35;
+  private static final int BOX_HEIGHT_TWO = 58;
   
   /** The default box width. */
   private static final int BOX_WIDTH = 5;
   
   /** The default border, a black line. */
-  private static final Border BLACK_LINE = BorderFactory.createLineBorder(Color.BLACK, 3);
+  private static final Border BLACK_LINE = BorderFactory.createLineBorder(new JPanel().getBackground(), 15);
   
   /** The label that shows the auction's name. */
   private static final JLabel FORM_TITLE = new JLabel("Registration:");
 
+  /////////LABELS////////
+  
+  /** Label for username field. */
+  private final JLabel my_username_label = new JLabel("Username: ");
+
+  /** Label for password field. */
+  private final JLabel my_password_label = new JLabel("Password: ");
+  
+  /** Label for first name field. */
+  private final JLabel my_first_name_label = new JLabel("First name: ");
+
+  /** Label for last namefield. */
+  private final JLabel my_last_name_label = new JLabel("Last name: ");
+  
   /** Label for creditcard number field. */
   private final JLabel my_card_num_label = new JLabel("Card # ");
     
   /** Label for creditcard CSC field. */
   private final JLabel my_csc_label = new JLabel("CSC: ");
     
-  /** Label for contact person name. */
-  private final JLabel my_exp_date_label = new JLabel("Contact Person: ");
+  /** Label for creditcard expiration date field. */
+  private final JLabel my_exp_date_label= new JLabel("Expiration date:     ");
 
+  /////////TEXT FIELDS////////
   
-  /** The card number input field. */
-  private JFormattedTextField my_card_num_field = new JFormattedTextField();
+  /** Username field. */
+  private final JFormattedTextField my_username_field = new JFormattedTextField();
+
+  /** Password field. */
+  private final JFormattedTextField my_password_field = new JFormattedTextField();
   
-  /** The card CSC input field. */
-  private JFormattedTextField my_csc_field = new JFormattedTextField();
+  /** First name field. */
+  private final JFormattedTextField my_first_name_field = new JFormattedTextField();
+
+  /** Last name field. */
+  private final JFormattedTextField my_last_name_field = new JFormattedTextField();
   
-  /** The expiration date input field. */
-  private JFormattedTextField my_exp_date_field = new JFormattedTextField();
+  /** Card number input field. */
+  private final JFormattedTextField my_card_num_field = new JFormattedTextField();
   
+  /** Card CSC input field. */
+  private final JFormattedTextField my_csc_field = new JFormattedTextField();
+  
+  /** Expiration date input field. */
+  private final JFormattedTextField my_exp_date_field = new JFormattedTextField();
+  
+  /////////BUTTONS////////
   
   /** The button to click to go back to the main screen. */
   private final JButton my_back_button = new JButton("Back");
@@ -113,9 +150,15 @@ public class RegistrationPanel extends JPanel {
     
     
   }
-  
+
+  /**
+   * Tell accessibility tools about label/textfield pairs.
+   */
   private void setupInput() {
-    //Tell accessibility tools about label/textfield pairs.
+    my_username_label.setLabelFor(my_username_field);
+    my_password_label.setLabelFor(my_password_field);
+    my_first_name_label.setLabelFor(my_first_name_field);
+    my_last_name_label.setLabelFor(my_last_name_field);
     my_card_num_label.setLabelFor(my_card_num_field);
     my_csc_label.setLabelFor(my_csc_field);
     my_exp_date_label.setLabelFor(my_exp_date_field);
@@ -129,17 +172,34 @@ public class RegistrationPanel extends JPanel {
     final JPanel west = new JPanel();
     west.setLayout(new BoxLayout(west, BoxLayout.Y_AXIS));
     final JPanel north = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    final JPanel east = new JPanel(new GridLayout(0, 1));
+    final JPanel east = new JPanel(new GridLayout(1, 1));
+    east.setPreferredSize(new Dimension(120, 680));
     final JPanel south = new JPanel(new BorderLayout());
     final JPanel southcomment = new JPanel(new FlowLayout());
     final JPanel southset = new JPanel(new FlowLayout());
     
     FORM_TITLE.setFont(new Font(ARIAL, Font.BOLD, MAIN_FONT_SIZE));
+    my_username_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
+    my_password_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
+    my_first_name_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
+    my_last_name_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
     my_card_num_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
     my_csc_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
     my_exp_date_label.setFont(new Font(ARIAL, Font.BOLD, INSTRUCTION_FONT_SIZE));
-    
+        
+
     north.add(FORM_TITLE, BorderLayout.CENTER);
+
+   
+    west.add(my_username_label);
+    west.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT)));
+    west.add(my_password_label);
+    west.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT)));
+    west.add(my_first_name_label);
+    west.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT)));
+    west.add(my_last_name_label);
+    west.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT)));
+
    
     
 //    final JPanel auctionnum = new JPanel(new FlowLayout());
@@ -147,6 +207,7 @@ public class RegistrationPanel extends JPanel {
 //    auctionnum.add(my_number);
 //    east.add(auctionnum);
     
+
     west.add(my_card_num_label);
     west.add(Box.createRigidArea(new Dimension(BOX_WIDTH, 172)));
     west.add(my_csc_label);
@@ -171,6 +232,14 @@ public class RegistrationPanel extends JPanel {
     final JPanel textinput = new JPanel();
     textinput.setLayout(new BoxLayout(textinput, BoxLayout.Y_AXIS));
     
+    textinput.add(my_username_field);
+    textinput.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT_TWO)));
+    textinput.add(my_password_field);
+    textinput.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT_TWO)));
+    textinput.add(my_first_name_field);
+    textinput.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT_TWO)));
+    textinput.add(my_last_name_field);
+    textinput.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT_TWO)));
     textinput.add(my_card_num_field);
     textinput.add(Box.createRigidArea(new Dimension(BOX_WIDTH, BOX_HEIGHT_TWO)));
     textinput.add(my_csc_field);
@@ -204,12 +273,30 @@ public class RegistrationPanel extends JPanel {
     my_save_button.setToolTipText("Save the card info");
     my_save_button.addActionListener(new ActionListener() {
       public void actionPerformed(final ActionEvent the_event) {
-        saveCard();
+        saveUser();
       }
     });
   }
 
-  private void saveCard() {
-    // TODO: Set Bidder to be "registered" and set their card data
+  private void saveUser() {
+
+    final String username = my_username_field.getText();
+    final String password = my_password_field.getText();
+    final String first_name = my_first_name_field.getText();
+    final String last_name = my_last_name_field.getText();
+    
+    final String card_num = my_card_num_field.getText();
+    final String csc = my_csc_field.getText();
+    final String exp_date = my_exp_date_field.getText();
+    
+    /*
+    if (fields are bad) {
+      show an error....
+    } else if () {
+      
+    } else {
+      my_system.addUser();
+    }
+    */
   }
 }
