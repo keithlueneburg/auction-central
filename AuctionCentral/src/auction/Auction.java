a89e94a50822a914c3a371a693faa059a68eba32
luenekw@uw.edu <luenekw@uw.edu@2b68a43b-548d-a8f3-15b1-0ecbf99f8650>
2014-03-14 06:17:07 +0000

    Comments/warnings in Auction

--- AuctionCentral/src/auction/Auction.java
+++ AuctionCentral/src/auction/Auction.java
@@ -19,54 +19,54 @@ public class Auction {
 
   // Used only when adding/removing items
   /**
-   * 
+   * The next item to be added to the auction.
    */
   private int my_next_item;
   /**
-   * 
+   * The auction number.
    */
   private int my_number;
   /**
-   * 
+   * The auction name.
    */
   private String my_auction_name;
   /**
-   * 
+   * The contact person name.
    */
   private String my_contact_person;
   /**
-   * 
+   * The contact phone number.
    */
   private String my_contact_phone;
   /**
-   * 
+   * The intake person name.
    */
   private String my_intake_person;
   /**
-   * 
+   * The auction date.
    */
   private Calendar my_auction_date;
   /**
-   * 
+   * The duration of the auction.
    */
   private int my_auction_duration;
   /**
-   * 
+   * The auction comments.
    */
   private String my_comments;
   /**
-   * 
+   * The list of the auction's items.
    */
   private List<Item> my_auction_items;
   /**
    * 
-   * @param the_auction_name
-   * @param the_contact_person
-   * @param the_contact_phone
-   * @param the_intake_person
-   * @param the_auction_date
-   * @param the_auction_duration
-   * @param the_comments
+   * @param the_auction_name Auction name.
+   * @param the_contact_person Contact person name.
+   * @param the_contact_phone Contact phone number.
+   * @param the_intake_person Intake person name.
+   * @param the_auction_date Auction date.
+   * @param the_auction_duration Duration of auction.
+   * @param the_comments Auction comments.
    */
   public Auction(final String the_auction_name,
       final String the_contact_person, final String the_contact_phone,
@@ -96,45 +96,57 @@ public class Auction {
   }
 
   /**
-   * Creates an auction assigned to the given user and their organization. 
+   * Creates an auction assigned to the given user and their organization.
+   * 
+   *  @param the_user The user logged in to the system.
    */
   public Auction(final NonProfitUser the_user) {
-    this(the_user.getOrganization(), the_user.getFirstName() + ' ' + the_user.getLastName(), "", "",
+    this(the_user.getOrganization(), the_user.getFirstName() + ' ' + the_user.getLastName(),
+        "", "",
         new GregorianCalendar(), 0, "");
   }
   
   // SETTERS
   /**
+   * Set the auction name.
    * 
-   * @param the_auction_name
+   * @param the_auction_name The name of the auction.
    */
   public void setAuctionName(final String the_auction_name) {
     my_auction_name = the_auction_name;
   }
+  
   /**
+   * Set the contact person name.
    * 
-   * @param the_contact_person
+   * @param the_contact_person Contact person name.
    */
   public void setContactPerson(final String the_contact_person) {
     my_contact_person = the_contact_person;
   }
+  
   /**
+   * Set the contact phone number.
    * 
-   * @param the_contact_phone
+   * @param the_contact_phone The contact phone number.
    */
   public void setContactPhone(final String the_contact_phone) {
     my_contact_phone = the_contact_phone;
   }
+  
   /**
+   * Set the intake person name.
    * 
-   * @param the_intake_person
+   * @param the_intake_person The intake person name.
    */
   public void setIntakePerson(final String the_intake_person) {
     my_intake_person = the_intake_person;
   }
+  
   /**
+   * Set the auction date.
    * 
-   * @param the_auction_date
+   * @param the_auction_date The auction date.
    */
   public void setAuctionDate(final Calendar the_auction_date) {
     my_auction_date = the_auction_date;
@@ -145,16 +157,19 @@ public class Auction {
     
     setAuctionName(my_auction_name + " - " + formatted_date);
   }
+  
   /**
+   * Set the auction duration.
    * 
-   * @param the_auction_duration
+   * @param the_auction_duration The auction duration.
    */
   public void setAuctionDuration(final int the_auction_duration) {
     my_auction_duration = the_auction_duration;
   }
   /**
+   * Set the auction comments.
    * 
-   * @param the_comments
+   * @param the_comments The auction comments.
    */
   public void setComments(final String the_comments) {
     my_comments = the_comments;
@@ -162,8 +177,9 @@ public class Auction {
 
   // GETTERs
   /**
+   * Get the auction name.
    * 
-   * @return
+   * @return the name.
    */
   public String getAuctionName() {
     return my_auction_name;
@@ -177,66 +193,90 @@ public class Auction {
    * @author Casey Morrison
    */
   public String getAuctionNumber() {
-    StringBuilder sb = new StringBuilder();
+    final StringBuilder sb = new StringBuilder();
     sb.append("");
     sb.append(my_number);
-    String strI = sb.toString();
-    return strI;
+    final String str = sb.toString();
+    return str;
   }
+  
   /**
+   * Get the contact person name.
    * 
-   * @return
+   * @return Contact person name.
    */
   public String getContactPerson() {
     return my_contact_person;
   }
+  
   /**
+   * Get the contact phone number.
    * 
-   * @return
+   * @return the phone number.
    */
   public String getContactPhone() {
     return my_contact_phone;
   }
+  
   /**
+   * Get intake person name.
    * 
-   * @return
+   * @return the intake person.
    */
   public String getIntakePerson() {
     return my_intake_person;
   }
+  
   /**
+   * Get the auction date.
    * 
-   * @return
+   * @return the date.
    */
   public Calendar getAuctionDate() {
     return my_auction_date;
   }
+  
   /**
+   * Get auction duration.
    * 
-   * @return
+   * @return the duration.
    */
   public int getAuctionDuration() {
     return my_auction_duration;
   }
+  
   /**
+   * Get auction comments.
    * 
-   * @return
+   * @return the comments.
    */
   public String getComments() {
     return my_comments;
   }
+  
   /**
+   * Get the item count.
    * 
-   * @return
+   * @return the item count.
    */
   public int getItemCount() {
     return my_auction_items.size();
   }
   
+  /**
+   * Get the list of item belonging to the auction.
+   * 
+   * @return the list of items.
+   */
   public List<Item> getItems() {
     return my_auction_items;
   }
   
+  /**
+   * Set the items to the given list of items.
+   * 
+   * @param an_item_list The list to set the auction's items to.
+   */
   public void setItems(final List<Item> an_item_list) {
     my_auction_items = an_item_list;
   }
